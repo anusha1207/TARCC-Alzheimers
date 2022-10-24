44087f55600a70b46c7bb608be2095ac87ddd21e
sophiaprieto <sbp3@rice.edu>
2022-10-23 19:21:50 -0500

    initial modules

--- modeling.py
+++ modeling.py
@@ -1,6 +1,46 @@
 import preprocessing as pp
+!pip install kmodes
+!pip install xgboost
+!pip install lightgbm
+!pip install mlxtend --upgrade
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import os
+import warnings
+from kmodes.kmodes import KModes
+from pandas_profiling import ProfileReport
+import statsmodels.api as sm
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression #linear_model.LogisticRegression (setting multi_class=”multinomial”)
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.feature_selection import RFE
+from sklearn.svm import SVR
+# from sklearn import externals
+# import joblib
+from mlxtend.feature_selection import SequentialFeatureSelector as sfs
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBClassifier
+import lightgbm as lgbm
+from sklearn.datasets import make_blobs
+%matplotlib inline
+warnings.filterwarnings("ignore")
 
 ### Load data
 non_genetic_data = pd.read_csv("20220916 updated TARCC Data for Dr Broussard.csv")
 
 df = pp.preprocessing(non_genetic_data)
+

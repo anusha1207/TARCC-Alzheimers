81e412fb9d72adc73c0e179df5d4261219a50eaf
Angela Cao <runningnerdtroll@gmail.com>
2022-11-01 13:51:06 -0500

    Updating Deep NN modularized class
    Updating Deep NN modularized class with two new model implementations (more layers and/or nodes per layer) and adjusting output classes to match number of intended classes.

--- Deep_NN.py
+++ Deep_NN.py
@@ -4,6 +4,7 @@ from keras.utils.np_utils import to_categorical
 from sklearn.model_selection import cross_val_score
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import KFold
+import numpy as np
 
 # Code source: https://machinelearningmastery.com/multi-class-classification-tutorial-keras-deep-learning-library/
 # Code source 2: https://www.analyticsvidhya.com/blog/2021/07/performing-multi-class-classification-on-fifa-dataset-using-keras/
@@ -15,6 +16,7 @@ def DNN_One(input, output, epochs=200, batch_size=5, n_split=5):
 
     # Convert output to categorical variables
     cat_output = to_categorical(output)
+    cat_output = np.delete(cat_output, 0, axis=1)
 
     def DNN_model():
         model = Sequential()
@@ -42,6 +44,7 @@ def DNN_Two(input, output, epochs=200, batch_size=5, n_split=5):
 
   # Convert output to categorical variables
   cat_output = to_categorical(output)
+  cat_output = np.delete(cat_output, 0, axis=1)
 
   def DNN_model():
     model = Sequential()
@@ -71,6 +74,7 @@ def DNN_Three(input, output, epochs=200, batch_size=5, n_split=5):
 
   # Convert output to categorical variables
   cat_output = to_categorical(output)
+  cat_output = np.delete(cat_output, 0, axis=1)
 
   def DNN_model():
     model = Sequential()
@@ -96,17 +100,78 @@ def DNN_Four(input, output, epochs=200, batch_size=5, n_split=5):
 
   # Convert output to categorical variables
   cat_output = to_categorical(output)
+  cat_output = np.delete(cat_output, 0, axis=1)
 
   def DNN_model():
     model = Sequential()
     # First layer of nodes of number of columns/features in dataset and then hidden layer of 128 nodes
     model.add(Dense(128, input_shape=(len(input.columns), ), activation='relu'))
-    # Another hidden layer of 64 nodes
+    # Another hidden layer of 128 nodes
     model.add(Dense(128, activation='relu'))
-    # Another hidden layer of 64 nodes
+    # Another hidden layer of 128 nodes
     model.add(Dense(128, activation='relu'))
     # Output layer of number of output classes (number of unique values of 'P1_PT_TYPE' column/feature)
     model.add(Dense(len(output.unique()) + 1, activation='softmax'))
+    # Use categorical_crossentropy for loss since this is a classification problem
+    # Use ADAM optimizer since we are dealing with a large dataset (in terms of number of rows and features)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+  # Training and Testing Phase; using Cross Validation to evaluate model
+  estimator = KerasClassifier(build_fn=DNN_model, epochs=epochs, batch_size=batch_size, verbose=1)
+  kfold = KFold(n_splits=n_split, shuffle=True)
+  results = cross_val_score(estimator, input_float, cat_output, cv=kfold, error_score='raise')
+  return results.mean()
+
+def DNN_Five(input, output, epochs=200, batch_size=5, n_split=5):
+  # Data Processing on Input so that we can fit the input into our neural network training
+  input_values = input.values
+  input_float = input_values.astype(float)
+
+  # Convert output to categorical variables
+  cat_output = to_categorical(output)
+  cat_output = np.delete(cat_output, 0, axis=1)
+
+  def DNN_model():
+    model = Sequential()
+    # First layer of nodes of number of columns/features in dataset and then hidden layer of 512 nodes
+    model.add(Dense(512, input_shape=(len(input.columns), ), activation='relu'))
+    # Another hidden layer of 1024 nodes
+    model.add(Dense(1024, activation='relu'))
+    # Another hidden layer of 1024 nodes
+    model.add(Dense(1024, activation='relu'))
+    # Another hidden layer of 1024 nodes
+    model.add(Dense(1024, activation='relu'))
+    # Another hidden layer of 512 nodes
+    model.add(Dense(512, activation='relu'))
+    # Output layer of number of output classes (number of unique values of 'P1_PT_TYPE' column/feature)
+    model.add(Dense(len(output.unique()), activation='softmax'))
+    # Use categorical_crossentropy for loss since this is a classification problem
+    # Use ADAM optimizer since we are dealing with a large dataset (in terms of number of rows and features)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+  # Training and Testing Phase; using Cross Validation to evaluate model
+  estimator = KerasClassifier(build_fn=DNN_model, epochs=epochs, batch_size=batch_size, verbose=1)
+  kfold = KFold(n_splits=n_split, shuffle=True)
+  results = cross_val_score(estimator, input_float, cat_output, cv=kfold, error_score='raise')
+  return results.mean()
+
+def DNN_Six(input, output, epochs=200, batch_size=5, n_split=5):
+  # Data Processing on Input so that we can fit the input into our neural network training
+  input_values = input.values
+  input_float = input_values.astype(float)
+
+  # Convert output to categorical variables
+  cat_output = to_categorical(output)
+  cat_output = np.delete(cat_output, 0, axis=1)
+
+  def DNN_model():
+    model = Sequential()
+    # First layer of nodes of number of columns/features in dataset and then hidden layer of 1024 nodes
+    model.add(Dense(1024, input_shape=(len(input.columns), ), activation='relu'))
+    # Output layer of number of output classes (number of unique values of 'P1_PT_TYPE' column/feature)
+    model.add(Dense(len(output.unique()), activation='softmax'))
     # Use categorical_crossentropy for loss since this is a classification problem
     # Use ADAM optimizer since we are dealing with a large dataset (in terms of number of rows and features)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])

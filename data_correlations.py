ec437b688477a75225f05cab6b54057d25977688
xanderabbott <ada5@rice.edu>
2023-02-14 12:18:16 -0600

    added corrections to proteo column names

--- data_correlations.py
+++ data_correlations.py
@@ -13,13 +13,17 @@ df = get_cleaned_data()
 corr_values = df.corr()
 np.fill_diagonal(corr_values.values, 0)
 
-# Scan each row to see if it has any correlations over 0.5 or under -0.5. If not, add to list to delete
+# Scan each row to see if it has any correlations over 0.75 or under -0.75. If not, add to list to delete
+for column in corr_values:
+    corr_values[column].mask(corr_values[column] >= 1.0, np.nan, inplace=True)
+    corr_values[column].mask(corr_values[column] <= -1.0, np.nan, inplace=True)
+        
 
 rows_delete = []
 for index, row in corr_values.iterrows():
     delete = True
     for value in row: 
-        if value >= 0.5 or value <= -0.5:
+        if (value >= 0.5 and value < 0.995) or (value <= -0.5 and value > -0.995):
             delete = False
     rows_delete.append(tuple([index, delete]))
 
@@ -30,13 +34,48 @@ for row in rows_delete:
     if row[1]:
         corr_values = corr_values.iloc[corr_values.index!=row[0], corr_values.columns!=row[0]]
 
+corr_values.shape
+
+# following two functions identify top correlation pairs
+
+def get_redundant_pairs(df):
+    '''Get diagonal and lower triangular pairs of correlation matrix'''
+    pairs_to_drop = set()
+    cols = df.columns
+    for i in range(0, df.shape[1]):
+        for j in range(0, i+1):
+            pairs_to_drop.add((cols[i], cols[j]))
+    return pairs_to_drop
+
+def get_top_abs_correlations(df, n=5):
+    au_corr = df.abs().unstack()
+    labels_to_drop = get_redundant_pairs(df)
+    au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
+    return au_corr[0:n]
+
+# isolating top 65 pairs to optimize feature visibility
+top = get_top_abs_correlations(corr_values, 65)
+
+# recording all rows + columns of interest
+topdf = top.unstack(level=-1)
+features = set(list(topdf.columns) + topdf.axes[0].tolist())
+len(features)
+
+# filtering out nonimportant features
+
+for col in corr_values.columns:
+    if col not in features:
+        corr_values = corr_values.iloc[corr_values.index != col, corr_values.columns != col]
+
+corr_values.shape
+
 # Fill diagonal and upper half with NaNs
 mask = np.zeros_like(corr_values, dtype=bool)
 mask[np.triu_indices_from(mask)] = True
 corr_values[mask] = np.nan
 f = plt.figure(figsize=(19, 15))
 plt.matshow(corr_values, fignum=f.number)
-plt.xticks(range(corr_values.select_dtypes(['number']).shape[1]), corr_values.select_dtypes(['number']).columns, fontsize=14, rotation=45)
+plt.xticks(range(corr_values.select_dtypes(['number']).shape[1]), corr_values.select_dtypes(['number']).columns, fontsize=14, rotation=90)
 plt.yticks(range(corr_values.select_dtypes(['number']).shape[1]), corr_values.select_dtypes(['number']).columns, fontsize=14)
 cb = plt.colorbar()
 cb.ax.tick_params(labelsize=14)
--- data_labels.py
+++ data_labels.py
@@ -4,9 +4,35 @@ import pandas as pd
 
 codebook = pd.read_excel("20220602-TARCC-Codebook.xlsx")
 
-def get_desc_table(cols) -> dict:
-    descs = {}
+# TODO: create dictionary to direct proteo names in csv to proteo names in codebook
+
+names_to_codebook_proteo = {"PROTEO_ADIPONECTIN" : "Proteomics_Adiponectin", "PROTEO_BFGF" : "Proteomics_bFGF", "PROTEO_CLUSTERIN" : "Proteomics_Clusterin",
+"PROTEO_CRP" : "Proteomics_CRP", "PROTEO_EOTAXIN_HUMAN" : "Proteomics_Eotaxin (Human)", "PROTEO_EOTAXIN_3_HUMAN" : "Proteomics_Eotaxin-3 (Human)", "PROTEO_FABP3" : "Proteomics_FABP3",
+"PROTEO_FACTOR_VII" : "Proteomics_Factor VII", "PROTEO_FLT_1" : "Proteomics_Flt-1", "PROTEO_GLUCAGON" : "Proteomics_Glucagon", "PROTEO_GM_CSF_HUMAN" : "Proteomics_GM-CSF (Human)",
+"PROTEO_IFN_Y_HUMAN" : "Proteomics_IFN-y (Human)", "PROTEO_IL_10_HUMAN" : "Proteomics_IL-10 (Human)", "PROTEO_IL_12_P40_HUMAN" : "Proteomics_IL-12 p40 (Human)", 
+"PROTEO_IL_12_P70_HUMAN" : "Proteomics_IL-12 p70 (Human)", "PROTEO_IL_13_HUMAN" : "Proteomics_IL-13 (Human)", "PROTEO_IL_15" : "Proteomics_IL-15", "PROTEO_IL_16" : "Proteomics_IL-16",
+"PROTEO_IL_17A" : "Proteomics_IL-17A", "PROTEO_IL_1A" : "Proteomics_IL-1a", "PROTEO_IL_1B_HUMAN" : "Proteomics_IL-1B (Human)", "PROTEO_IL_2_HUMAN" : "Proteomics_IL-2 (Human)",
+"PROTEO_IL_4_HUMAN" : "Proteomics_IL-4 (Human)", "PROTEO_IL_5_HUMAN" : "Proteomics_IL-5 (Human)", "PROTEO_IL_6_HUMAN" : "Proteomics_IL-6 (Human)", "PROTEO_IL_7_HUMAN" : "Proteomics_IL-7 (Human)", 
+"PROTEO_IL_8_HUMAN" : "Proteomics_IL-8 (Human)", "PROTEO_IL_8_HA" : "Proteomics_IL-8 HA", "PROTEO_INSULIN" : "Proteomics_Insulin", "PROTEO_IP_10_HUMAN" : "Proteomics_IP-10 (Human)",
+"PROTEO_LBP" : "Proteomics_LBP", "PROTEO_LEPTIN" : "Proteomics_Leptin", "PROTEO_MCP_1_HUMAN" : "Proteomics_MCP-1 (Human)", "PROTEO_MCP_4_HUMAN" : "Proteomics_MCP-4 (Human)",
+"PROTEO_MDC_HUMAN" : "Proteomics_MDC (Human)", "PROTEO_MIP_1A_HUMAN" : "Proteomics_MIP-1a (Human)", "PROTEO_MIP_1B_HUMAN" : "Proteomics_MIP-1B (Human)", "PROTEO_MPO" : "Proteomics_MPO", 
+"PROTEO_NT_PROBNP" : "Proteomics_NT-proBNP", "PROTEO_PIGF" : "Proteomics_PIGF", "PROTEO_PYY" : "Proteomics_PYY", "PROTEO_RESISTIN" : "Proteomics_Resistin", "PROTEO_SAA" : "Proteomics_SAA",
+"PROTEO_SICAM_1" : "Proteomics_sICAM-1", "PROTEO_SVCAM_1" : "Proteomics_sVCAM-1", "PROTEO_TARC_HUMAN" : "Proteomics_TARC (Human)", "PROTEO_TIE_2" : "Proteomics_TIE-2", 
+"PROTEO_TNF_A_HUMAN" : "Proteomics_TNF-a (Human)", "PROTEO_TNF_B" : "Proteomics_TNF-B", "PROTEO_TOTAL_GIP" : "Proteomics_Total GIP", "PROTEO_VEGF_ANGIO_PLATE" : "Proteomics_VEGF- angio plate", 
+"PROTEO_VEGF_CYTO_PLATE" : "Proteomics_VEGF- cyto plate", "PROTEO_VEGF_C" : "Proteomics_VEGF-C", "PROTEO_VEGF_D" : "Proteomics_VEGF-D"}
+
+def get_desc_table(cols):
+    descs = []
     for col in cols:
-        row = codebook.query(f"`Variable Name` == '{col}'")
-        descs[col] = row["Description"][0]
+        entry = {}
+        col = str(col).upper()
+        row = codebook[codebook["Variable Name"] == col]
+        entry["Variable Name"] = col
+        entry["Description"] = row["Description"].values
+        descs.append(entry)
+    for entry in descs:
+        if len(entry["Description"]) != 0:
+            entry["Description"] = entry["Description"][0]
+        else:
+            entry["Description"] = "Incompatible"
     return descs

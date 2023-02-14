4602e6e505f4fe756a8288af88cc5f7bfb387d78
xanderabbott <ada5@rice.edu>
2023-02-13 19:58:55 -0600

    data labels production

--- data_labels.py
+++ data_labels.py
@@ -4,13 +4,9 @@ import pandas as pd
 
 codebook = pd.read_excel("20220602-TARCC-Codebook.xlsx")
 
-def get_desc(col) -> str:
-    q = f"`Variable Name` == '{col}'"
-    row = codebook.query(q)
-    return row["Description"][0]
-
 def get_desc_table(cols) -> dict:
     descs = {}
     for col in cols:
-        descs[col] = get_desc(col)
+        row = codebook.query(f"`Variable Name` == '{col}'")
+        descs[col] = row["Description"][0]
     return descs

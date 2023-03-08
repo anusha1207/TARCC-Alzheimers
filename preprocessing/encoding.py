fc6bc1e37d3a8d688a856058a014ff072736f6c5
Roy Montemayor <rfm5@rice.edu>
2023-03-07 19:00:58 -0600

    Added Patient Type Encoding.

--- preprocessing/encoding.py
+++ preprocessing/encoding.py
@@ -16,6 +16,18 @@ def encode_demographics(df: pd.DataFrame) -> pd.DataFrame:
     """
     return pd.get_dummies(df, dummy_na=True, columns=['A1_HISPANIC', 'A1_MARISTAT', 'A1_RACE', 'A1_SEX'])
 
+def encode_patient_type(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Encodes the type the patient was assigned for that visit (P1) of the input dataframe.
+
+    Args:
+        df: The cleaned dataframe representing the TARCC dataset.
+
+    Returns:
+        A dataframe with the A1 categorical variables encoded.
+    """
+
+    return pd.get_dummies(df, dummy_na=True, columns=["P1_PT_TYPE"])
 
 def encode_data(df: pd.DataFrame) -> pd.DataFrame:
     """

1e53593398727cec30538e4c2891a86e20602ca6
Fadeel Sher Khan <fk13@rice.edu>
2023-03-22 14:36:36 -0500

    Updated requirements.txt with mrmr_selection and sklearn

--- modeling/mrmr.py
+++ modeling/mrmr.py
@@ -1,13 +1,13 @@
 import mrmr
-from mrmr import mrmr_classif
+import mrmr
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.datasets import make_classification
 
-def perform_mrmr(X: pd.DataFrame, y: pd.Series, k: int):
-    selected_features = mrmr_classif(X=X, y=y, K=k)
+def perform_mrmr(X: pd.DataFrame, y: pd.Series, k: int, scr):
+    selected_features = mrmr.mrmr_classif(X=X, y=y, K=k, return_scores= scr)
     return selected_features
 
 

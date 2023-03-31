68d588c95034dca8f371ad37fdbb1c7b5c172566
xanderabbott <ada5@rice.edu>
2023-03-31 13:49:44 -0500

    RF edit

--- modeling/RandomForest.py
+++ modeling/RandomForest.py
@@ -6,20 +6,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, f1_score
-# import sys
-# sys.path.insert(0, "\xabbo\Desktop\TARCC\TARCC_F22\preprocessing")
-from preprocessing.data_cleaning import get_cleaned_data
 
 
-
-
-def run_random_forest(df: pd.DataFrame, num_iters: int = 1):
+def run_random_forest(df: pd.DataFrame, num_iters: int = 1, blood: bool = False):
     """
     Runs a random forest model on the input dataframe, using "P1_PT_TYPE" as the label.
 
     Args:
         df: The cleaned and encoded TARCC dataset.
         num_iters: The number of random forest iterations to perform.
+        blood: boolean to indicate whether blood data is ran. If so, eliminates the MCI response variable
 
     Returns:
 
@@ -31,21 +27,21 @@ def run_random_forest(df: pd.DataFrame, num_iters: int = 1):
         data = data.drop("PATID", axis=1)
     if "RBM_TARC_PID" in df.columns:
         data = data.drop("RBM_TARC_PID", axis=1)
+    if blood:
+        data = data[data[LABEL] != 4]
 
     features = data.drop(LABEL, axis=1).columns
 
     X = data.drop(LABEL, axis=1).values
     y = data[LABEL]
 
-    # KNN Imputation to 
     imputer = KNNImputer(keep_empty_features=True)
     X = imputer.fit_transform(X)
 
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
 
-    #best_cs = np.zeros(num_iters)
-    #best_l1_ratios = np.zeros(num_iters)
+    
     micro_f1_scores = np.zeros(num_iters)
     feature_importances = []
     confusion_matrices = []
@@ -74,8 +70,3 @@ def run_random_forest(df: pd.DataFrame, num_iters: int = 1):
 
     return micro_f1_scores, feature_importances, confusion_matrices
 
-# df = encode_data(get_cleaned_data())
-# df = df[df["P1_PT_TYPE"] != 3]
-# blood, clinical = split_csv(df)
-
-# micro_f1_scores, feature_importances, confusion_matrices = run_random_forest(clinical)

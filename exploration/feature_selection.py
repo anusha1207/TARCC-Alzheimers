7e7f10d5cdbe986caadf17455ef7efdd8decc410
michellelee <mhl6@rice.edu>
2023-04-10 12:01:36 -0500

    scaled plots

--- exploration/feature_selection.py
+++ exploration/feature_selection.py
@@ -24,20 +24,66 @@ def plot_mrmr_features(scores):
     plt.ylabel("Accuracy Score")
     plt.show()
 
+def plot_mrmr_features_scaled(scores):
+    scores = scores.sort_values(ascending=False)
+    scores = scores.head(10)
+    max_score = max(scores)
+    scores = scores.div(max_score)
+
+    ax = scores.plot(kind='bar')
+    plt.xticks(rotation=90)
+    plt.ylabel("Accuracy Score")
+    plt.show()
+
 
 def plot_rf_features(result, features):
     top_10_idx = np.argsort(result.importances_mean)[::-1][:10]
     top_10_features = features[top_10_idx]
     top_10_scores = result.importances_mean[top_10_idx]
+    top_10_std = result.importances_std[top_10_idx]
+
+
 
     # Print the top 10 features and their scores
     print("Top 10 features by permutation importance:")
     for feature, score in zip(top_10_features, top_10_scores):
         print(f"{feature}: {score}")
 
+
     plt.bar(top_10_features, top_10_scores)
     plt.xticks(rotation=90)
     plt.xlabel("Feature")
     plt.ylabel("Permutation importance score")
     plt.title("Top 10 features by permutation importance")
+    plt.show()
+
+    plt.boxplot(top_10_features, top_10_std)
+    plt.xticks(rotation = 90)
+    plt.show()
+
+def plot_rf_features_scaled(result, features):
+    top_10_idx = np.argsort(result.importances_mean)[::-1][:10]
+    top_10_features = features[top_10_idx]
+    top_10_scores = result.importances_mean[top_10_idx]
+    max_score = max(top_10_scores)
+    top_10_scores = top_10_scores.div(max_score)
+    top_10_std = result.importances_std[top_10_idx]
+
+
+
+    # Print the top 10 features and their scores
+    print("Top 10 features by permutation importance:")
+    for feature, score in zip(top_10_features, top_10_scores):
+        print(f"{feature}: {score}")
+
+
+    plt.bar(top_10_features, top_10_scores)
+    plt.xticks(rotation=90)
+    plt.xlabel("Feature")
+    plt.ylabel("Permutation importance score")
+    plt.title("Top 10 features by permutation importance")
+    plt.show()
+
+    plt.boxplot(top_10_features, top_10_std)
+    plt.xticks(rotation = 90)
     plt.show()
